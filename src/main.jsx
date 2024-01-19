6d9f2fbd22c78a157bfeae48e2d5fb58f7edfc2a
jsweetpotato37 <jsweetpotato37@gmail.com>
2024-01-19 11:18:16 +0900

    rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
 import React from "https://esm.sh/react";
 import { createRoot } from "https://esm.sh/react-dom";
 
+const anotherData = {
+  greetingMessage: ["행복한 아침이야", "아~ 상쾌한 아침"],
+  message: ["ㅋㅋ.."],
+};
+const data = {
+  greetingMessage: ["행복한 아침이야", "오늘도 좋은 하루 되렴~"],
+  message: ["나날이 성장하는 여러분을 보는 즐거움이 쏠쏠~", "ㅋㅋ.."],
+};
+
+// const greet = `안녕~ ${data.greetingMessage[1]}`;
+
+// 데이터 전환
+
+let mode = "data";
+
+// react는 slot 대신 중괄호를 쓴다. -> react명세에는 slot이라는 단어를 쓰진 않지만 웹 표준에서 slot이라는 키워드역할을 한다 할 수 있다.
 const createApp = () => (
   <div id="app">
     <h1>
-      안녕!
+      {data.greetingMessage[0]}
       <br />
-      리액트.
+      {data.greetingMessage[1]}
     </h1>
-    <p>리액트는 사용자 인터페이스 구축을 위한 JavaScript 오픈소스 라이브러리입니다.</p>
+    <p>{data.message}</p>
   </div>
 );
 
@@ -16,4 +32,16 @@ const rootElement = document.getElementById("root");
 const reactDomRoot = createRoot(rootElement);
 
 /* 함수 실행 => JSX -> React.createElement() -> 리액트 요소 : ReactElement */
-reactDomRoot.render(createApp());
+reactDomRoot.render(createApp(mode === "data" ? data : anotherData));
+
+// 버튼 이벤트 핸들링
+const button = document.querySelector("button");
+
+const handleChangeMessage = () => {
+  if (mode.includes("data")) mode = "anotherData";
+  else mode = "data";
+
+  console.log(mode);
+};
+
+button.addEventListener("click", handleChangeMessage);
